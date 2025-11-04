d051c4b7009161f63e3ef9735192fa2b867f1b97
Captain Coder <captaincodersacademy@gmail.com>
2025-11-03 18:12:37 -0700

    chore: Clean up dependencies.

--- minimal-prototype/vite.config.ts
+++ minimal-prototype/vite.config.ts
@@ -1,26 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    wasm(),
-    topLevelAwait()
+    react()
   ],
-  resolve: {
-    alias: {
-      // Provide polyfill for Node.js 'util' module
-      'util': path.resolve(__dirname, './src/utils/util-polyfill.ts')
-    }
-  },
-  optimizeDeps: {
-    include: ['opl3/lib/opl3'],  // Force pre-bundle with alias
-    exclude: ['@malvineous/opl']
-  },
   server: {
     fs: {
       // Allow serving files from node_modules
